c7ad4e267b18f9f0ead289a6d1cd2377bf675f4c
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-02 01:10:32 +0000

    start cli stub

---
+++ eden_events/lib/eden_events.rb
@@ -0,0 +1,11 @@
+require_relative "eden_events/version"
+require_relative "eden_events/cli"
+
+require 'pry'
+
+module EdenEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+#will be renaming to environment.rb
---
+++ eden_events/notes.md
@@ -0,0 +1,13 @@
+1. flow
+2. classes
+3. class relatiions
+4. 
+- user is asked for month
+- user sees events for month
+- user selects events
+- user sees event details
+
+classes
+-Events
+-CLI
+-Scraper
