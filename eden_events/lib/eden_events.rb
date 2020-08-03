eaf888e93bdd097e6f4d31765bcf21869c433adf
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-03 04:02:56 +0000

    some description

--- eden_events/lib/eden_events.rb
+++ eden_events/lib/eden_events.rb
@@ -2,6 +2,7 @@ require_relative "./eden_events/version"
 require_relative "./eden_events/cli"
 
 require 'pry'
+
 module EdenEvents
   class Error < StandardError; end
   # Your code goes here...
--- eden_events/lib/eden_events/cli.rb
+++ eden_events/lib/eden_events/cli.rb
@@ -1,6 +1,9 @@
 class EdenEvents::CLI
   def call
     puts 'Welcome to Eden!'
+    get_months
+    list_months
+    get_user_month
   end
   
   def get_months
@@ -10,5 +13,10 @@ class EdenEvents::CLI
   def list_months
     @months.each.with_index(1) do |month, index|
       puts "#{index +1}. #{month}" end
+  end
+  
+  def get_user_month
+    chosen_month = gets.strip
+    binding.pry
   end
 end
