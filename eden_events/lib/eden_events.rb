8bd96431e6d7a4e583108fea68efd6e2221bd483
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-03 03:15:22 +0000

    start cli stub

--- eden_events/lib/eden_events.rb
+++ eden_events/lib/eden_events.rb
@@ -1,5 +1,7 @@
-require "eden_events/version"
+require_relative "./eden_events/version"
+require_relative "./eden_events/cli"
 
+require 'pry'
 module EdenEvents
   class Error < StandardError; end
   # Your code goes here...
--- eden_events/lib/eden_events/cli.rb
+++ eden_events/lib/eden_events/cli.rb
@@ -1,20 +1,14 @@
 class EdenEvents::CLI
-  
   def call
-    puts "Welcome to Eden"
-    get_months
-    list_months
-    get_ user_month
+    puts 'Welcome to Eden!'
   end
   
   def get_months
-    @months.each.with_index(1) do |month, index|
-      puts '#{index}. #{month}'
-    end
+    @months = ['J','A','S','D']
   end
   
-  def get_user_month
-    chosen_month = gets.strip
-    binding.pry
+  def list_months
+    @months.each.with_index(1) do |month, index|
+      puts "#{index +1}. #{month}" end
   end
 end
