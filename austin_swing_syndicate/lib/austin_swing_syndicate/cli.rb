ee649f5093b42baf8ddde22886fff9b61a022dd8
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-04 10:43:25 +0000

    d

--- austin_swing_syndicate/lib/austin_swing_syndicate/cli.rb
+++ austin_swing_syndicate/lib/austin_swing_syndicate/cli.rb
@@ -1,32 +1,34 @@
 class AustinSwingSyndicate::CLI 
   def call
-    puts "\nWelcome to Austin Swing Syndicate\n"
-    get_events
+    puts "Welcome to Austin Swing Syndicate"
+    gets_events
     list_events
-    get_user_events
+    get_user_event
   end
   
-  def get_events
-    @events = [ "J", "A", "S", "D"]
+  def gets_events
+    puts "Choose an event for more information"
+    @events= [ "J", "A", "S", "D"]
   end
   
   def list_events
-    puts "Choose a event to see information"
-    @events.each.while_index(1) do |event, index|
+    @events.each.with_index(1) do |event, index|
       puts "#{index}. #{event}"
     end
   end
   
-  def get_user_events
-    chosen_events = gets.strip
-    binding.pry
-    # if valid_input(chosen_events,@events)
-    # end
+  def get_user_event
+    chosen_event = gets.strip.to_i
+    show_announcements_for(chosen_event) if valid_input(chosen_event,@events)
   end
   
-  def valid_input(input,data)
+  def valid_input(input, data)
     input.to_i <= data.length && input.to_i > 0
   end
   
-  
+  def show_announcements_for(chosen_event)
+    event = @events[chosen_event - 1]
+    puts "Here are our announcements for #{event}"
+    binding.pry
+  end
 end
