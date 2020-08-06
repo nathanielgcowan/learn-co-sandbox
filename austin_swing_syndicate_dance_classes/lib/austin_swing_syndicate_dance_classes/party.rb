1560078a5f151b1328445ccc8cc6161d69168c40
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-06 02:02:17 +0000

    this this and that

--- austin_swing_syndicate_dance_classes/lib/austin_swing_syndicate_dance_classes/party.rb
+++ austin_swing_syndicate_dance_classes/lib/austin_swing_syndicate_dance_classes/party.rb
@@ -1,15 +1,21 @@
 class AustinSwingSyndicateDanceClasses::Party
   @@all =[]
-  attr_accessor :name
+  attr_accessor :name :description
   
   def initialize(name)
     @name = name
+    @description = []
     save
   end
   
   def self.all
     AustinSwingSyndicateDanceClasses::Scraper.scrape_parties if @@all.empty?
     @@all
+  end
+  
+  def description
+    AustinSwingSyndicateDanceClasses::Scraper.scrape_description if @@all.empty?
+    @description
   end
   
   def save
