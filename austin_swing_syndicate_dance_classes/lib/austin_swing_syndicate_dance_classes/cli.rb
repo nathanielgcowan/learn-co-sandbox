85a1826c3c1911deba3f34b0e92616ebd6397ac1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-18 02:35:11 +0000

    this is the finished project

--- austin_swing_syndicate_dance_classes/lib/austin_swing_syndicate_dance_classes/cli.rb
+++ austin_swing_syndicate_dance_classes/lib/austin_swing_syndicate_dance_classes/cli.rb
@@ -12,6 +12,7 @@ class AustinSwingSyndicateDanceClasses::CLI
 "
     puts "Hello and welcome to Austin Swing Syndicate. We encourage others to learn how to swing dance."
     get_party
+    get_description
     list_party
     show_answer
   end
@@ -20,10 +21,18 @@ class AustinSwingSyndicateDanceClasses::CLI
     @parties = AustinSwingSyndicateDanceClasses::Party.all
   end
   
+  def get_description
+    @description = AustinSwingSyndicateDanceClasses::Description.all
+  end
+  
   def list_party
-    puts "Here are the available parties. Choose one below to see more information?"
-    @parties.each.with_index(1) do |party, index|
-      puts "#{index}. #{party.name}"
+    puts "Here are the available parties. Choose one below to see more information?".blue
+    new_list = []
+    @parties.each do |party|
+      new_list << "#{party.name}"
+    end
+    new_list.sort.each.with_index(1) do |i, index|
+      puts "#{index}. #{i}"
     end
   end
   
@@ -32,7 +41,7 @@ class AustinSwingSyndicateDanceClasses::CLI
     if valid_input(chosen_party, @parties)
       give_info_for(chosen_party)
     else
-      puts "Oops! Wrong answer. Please try again".red
+      puts "ERROR!!! Invalid input, please try again.".red
       list_party
       show_answer
     end
